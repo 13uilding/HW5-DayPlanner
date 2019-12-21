de42e00cbd1478ffdace9a5178d04a188882a584
deep <hereisdanbrown@gmail.com>
2019-12-21 12:57:25 -0700

    Used moment.js to modify the text on indext.html

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-console.log("HI");
-console.log(moment());
+var now = moment();
+var displayDateTime = now.format("LLLL");
+
+// Targets
+var pageTitle = $("#pageTitle");
+var pageMonth = $("#pageMonth");
+var pageNow = $("#pageNow");
+
+// Changing values
+pageMonth.text(now.format("ddd, Do MMMM, YYYY"));
+pageNow.text(`Current Time--${now.format("kk:mm")}`) 
+// Create a toggle between military time and regular
+// I want to create a button to refresh the time displayed
+
+
+
+console.log(now.format("LLLL"));
+// Parsing Strings should use the String + Format because other browsers
+console.log(now.format("LLLL")); // Saturday, December 21, 2019 12:10 pm
+
+
+
+// 
