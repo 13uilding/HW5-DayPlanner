73b3d0e854f4073c8c711cafd51463ab359a8940
deebs <hereisdanbrown@gmail.com>
2019-12-21 13:42:00 -0700

    I added a toggle for the current time so it displays a 12h or 24h clock.

--- script.js
+++ script.js
@@ -5,10 +5,24 @@ var displayDateTime = now.format("LLLL");
 var pageTitle = $("#pageTitle");
 var pageMonth = $("#pageMonth");
 var pageNow = $("#pageNow");
+var pageNowState = "12h"
+pageNow.text(`Current Time--${now.format("LT")}`);
 
 // Changing values
 pageMonth.text(now.format("ddd, Do MMMM, YYYY"));
-pageNow.text(`Current Time--${now.format("kk:mm")}`) 
+
+pageNow.on("click", function(event){
+    console.log("heyo")
+    console.log(pageNow);
+    if ( pageNow.attr("data-state") === "12h" ){
+        pageNow.text(`Current Time--${now.format("kk:mm")}`);
+        pageNow.attr("data-state", "24h");
+    } else {
+        pageNow.text(`Current Time--${now.format("LT")}`);
+        pageNow.attr("data-state", "12h");
+    }
+
+})
 // Create a toggle between military time and regular
 // I want to create a button to refresh the time displayed
 
