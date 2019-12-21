938d587b0724b26276ac8747be87ae34aef252bc
deebs <hereisdanbrown@gmail.com>
2019-12-21 15:00:43 -0700

    Added the dynamic color element to each of the todos. If the current time > time block it is grey, if same then red, and else it is blue.

--- script.js
+++ script.js
@@ -33,8 +33,26 @@ function initializeTime(){
     now = moment();
     displayDateTime = now.format("LLLL");
     currentHour = now.format("kk")
+    console.log(currentHour)
     pageMonth.text(now.format("ddd, Do MMMM, YYYY"));
     pageNow.text(`Current Time--${now.format("LT")}`);
+    // Change the start and end for hours displayed
+    for ( var hour = 6; hour < 22; hour++ ){
+        assignBackgroundColor(currentHour, hour);
+    };
+}
+
+function assignBackgroundColor(currentHour, hour){
+    var hourBlock = $(`.t${hour}`);
+    if ( currentHour > hour ){
+        hourBlock.attr("style", "background-color: lightslategray");
+    } else if ( parseInt(currentHour) === parseInt(hour) ){
+        console.log("equal to current");
+        hourBlock.attr("style", "background-color: rosybrown");
+    } else {
+        console.log("greater than current");
+        hourBlock.attr("style", "background-color: cornflowerblue");
+    }
 }
 
 
